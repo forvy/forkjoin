8b03e48d7b50c395070bc00ecbcefa1eacf09f03
forvy <17746819+forvy@users.noreply.github.com>
2023-09-13 14:57:12 +0700

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,39 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component } from '@angular/core';
+import { forkJoin } from 'rxjs';
+
+interface User {
+  id: number;
+  email: string;
+}
+
+interface Post {
+  id: number;
+  title: string;
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'forkjoin';
+  USERS = 'https://jsonplaceholder.typicode.com/users/';
+  POSTS = 'https://jsonplaceholder.typicode.com/posts/';
+  data!: [User[], Post[]];
+
+  constructor(http: HttpClient) {
+    const users = http.get<User[]>(this.USERS);
+    const posts = http.get<Post[]>(this.POSTS);
+
+    forkJoin([users, posts])
+      .subscribe(res => {
+        this.data = res;
+        console.log('User and Post', res);
+      });
+  }
 }
